ef5143c50133501e21ee885b9bb44d8d87260f3d
Болховитинов Дмитрий <bolhovitinov_d@goldapple.ru>
2017-10-09 19:01:34 +0500

    fix search by brand (all)

--- src/app/match.product.form.component.ts
+++ src/app/match.product.form.component.ts
@@ -32,10 +32,21 @@ export class MatchProductFormComponent implements OnInit {
     asyncLetuProducts: Observable<any>;
     asyncIldeProducts: Observable<any>;
     asyncRiveProducts: Observable<any>;
+
     filteredMatchOptions: Observable<any>;
     filteredLetuMatchOptions: Observable<any>;
     filteredRiveMatchOptions: Observable<any>;
     filteredIldeMatchOptions: Observable<any>;
+
+    filteredTxtMatchOptions: Observable<any>;
+    filteredTxtLetuMatchOptions: Observable<any>;
+    filteredTxtRiveMatchOptions: Observable<any>;
+    filteredTxtIldeMatchOptions: Observable<any>;
+
+    asyncFulltextSearchGest: Observable<any>;
+    asyncFulltextSearchLetu: Observable<any>;
+    asyncFulltextSearchIlde: Observable<any>;
+    asyncFulltextSearchRive: Observable<any>;
 
     p: any;
     p1: any;
@@ -47,28 +58,24 @@ export class MatchProductFormComponent implements OnInit {
     totalMatch1: number;
     totalMatch2: number;
     totalMatch3: number;
+    totalMatch4: number;
+
     myCompleteMatchControl = new FormControl();
     myCompleteLetuMatchControl = new FormControl();
     myCompleteIldeMatchControl = new FormControl();
     myCompleteRiveMatchControl = new FormControl();
 
+    myCompleteGestFullTxtControl = new FormControl();
+    myCompleteLetuFullTxtControl = new FormControl();
+    myCompleteIldeFullTxtControl = new FormControl();
+    myCompleteRiveFullTxtControl = new FormControl();
+
     gestoriSelectedBrand: any;
+    riveSelectedBrand: any;
+    letuSelectedBrand: any;
+    ildeSelectedBrand: any;
 
-    filter(val: string): any {
-        return this.http.get('http://127.0.0.1:5000/gestori_brands?search='+val)
-    }
 
-    filterLetu(val: string): any {
-        return this.http.get('http://127.0.0.1:5000/all_brands?search='+val)
-    }
-
-    filterIlde(val: string): any {
-        return this.http.get('http://127.0.0.1:5000/all_brands?search='+val)
-    }
-
-    filterRive(val: string): any {
-        return this.http.get('http://127.0.0.1:5000/all_brands?search='+val)
-    }
 
     constructor(
         private http: HttpClient,
@@ -80,6 +87,7 @@ export class MatchProductFormComponent implements OnInit {
         this.dialog = dialog
         this.snackBar = snackBar
 
+        // autocomplete BRAND search
         this.filteredMatchOptions = this.myCompleteMatchControl.valueChanges
             .flatMap(val => this.http.get('http://127.0.0.1:5000/brands?s='+val+'&p=gest'));
 
@@ -91,6 +99,19 @@ export class MatchProductFormComponent implements OnInit {
 
         this.filteredRiveMatchOptions = this.myCompleteRiveMatchControl.valueChanges
             .flatMap(val => this.http.get('http://127.0.0.1:5000/brands?s='+val+'&p=rive'));
+
+        // autocomplete product search
+        this.filteredTxtMatchOptions = this.myCompleteMatchControl.valueChanges
+            .flatMap(val => this.http.get('http://127.0.0.1:5000/ft?s='+val+'&p=gest'));
+
+        this.filteredTxtLetuMatchOptions = this.myCompleteLetuMatchControl.valueChanges
+            .flatMap(val => this.http.get('http://127.0.0.1:5000/ft?s='+val+'&p=letu'));
+
+        this.filteredTxtIldeMatchOptions = this.myCompleteIldeMatchControl.valueChanges
+            .flatMap(val => this.http.get('http://127.0.0.1:5000/ft?s='+val+'&p=ilde'));
+
+        this.filteredTxtRiveMatchOptions = this.myCompleteRiveMatchControl.valueChanges
+            .flatMap(val => this.http.get('http://127.0.0.1:5000/ft?s='+val+'&p=rive'));
     }
 
     ngOnInit(): void {
@@ -103,6 +124,21 @@ export class MatchProductFormComponent implements OnInit {
     getGestoriBrands(event) {
         this.gestoriSelectedBrand = event.source.value;
         this.getPageGestori(1);
+    }
+
+    getRiveBrands(event) {
+        this.riveSelectedBrand = event.source.value;
+        this.getPageRive(1);
+    }
+
+    getIldeBrands(event) {
+        this.ildeSelectedBrand = event.source.value;
+        this.getPageIlde(1);
+    }
+
+    getLetuBrands(event) {
+        this.letuSelectedBrand = event.source.value;
+        this.getPageLetu(1);
     }
 
     getPageGestori(page: number) {
@@ -121,47 +157,46 @@ export class MatchProductFormComponent implements OnInit {
     }
 
     getPageLetu(page: number) {
-        this.asyncLetuProducts = this.serverCallLetuObservable(page)
+        this.asyncLetuProducts = this.serverCallLetuObservable(page, this.letuSelectedBrand)
             .do(res => {
                 this.totalMatch1 = res.count;
                 this.p1 = page;
             }).map(res => res.data);
     }
 
-    serverCallLetuObservable(page: number): Observable<any> {
+    serverCallLetuObservable(page: number, search: string): Observable<any> {
         const perPage1 = this.pageSize;
         const start1 = (page - 1) * perPage1;
         const end1 = start1 + perPage1;
-        return this.http.get('http://127.0.0.1:5000/letu_products?page='+page+'&perPage='+perPage1)
+        return this.http.get('http://127.0.0.1:5000/letu_products?page='+page+'&perPage='+perPage1+'&search='+search)
     }
 
     getPageIlde(page: number) {
-        this.asyncIldeProducts = this.serverCallIldeObservable(page)
+        this.asyncIldeProducts = this.serverCallIldeObservable(page, this.ildeSelectedBrand)
             .do(res => {
                 this.totalMatch2 = res.count;
                 this.p2 = page;
             }).map(res => res.data);
     }
 
-    serverCallIldeObservable(page: number): Observable<any> {
-        const perPage2 = this.pageSize;
-        const start2 = (page - 1) * perPage2;
-        const end2 = start2 + perPage2;
-        return this.http.get('http://127.0.0.1:5000/ilde_products?page='+page+'&perPage='+perPage2)
+    serverCallIldeObservable(page: number, search: string): Observable<any> {
+        const start2 = (page - 1) * this.pageSize;
+        const end2 = start2 + this.pageSize;
+        return this.http.get('http://127.0.0.1:5000/ilde_products?page='+page+'&perPage='+this.pageSize+'&search='+search)
     }
 
     getPageRive(page: number) {
-        this.asyncRiveProducts = this.serverCallRiveObservable(page)
+        this.asyncRiveProducts = this.serverCallRiveObservable(page, this.riveSelectedBrand)
             .do(res => {
                 this.totalMatch3 = res.count;
                 this.p3 = page;
             }).map(res => res.data);
     }
 
-    serverCallRiveObservable(page: number): Observable<any> {
+    serverCallRiveObservable(page: number, search: string): Observable<any> {
         const perPage3 = this.pageSize;
         const start3 = (page - 1) * perPage3;
         const end3 = start3 + perPage3;
-        return this.http.get('http://127.0.0.1:5000/rive_products?page='+page+'&perPage='+perPage3)
+        return this.http.get('http://127.0.0.1:5000/rive_products?page='+page+'&perPage='+perPage3+'&search='+search)
     }
 }
